9c3bc4d567f7c8cb78dbbc9a7557cf34af7977c2
mohamed <74468132+melm158@users.noreply.github.com>
2021-01-10 06:51:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+using System;
+
+namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.c1 = new System.Windows.Forms.PictureBox();
+            this.c2 = new System.Windows.Forms.PictureBox();
+            this.pirson2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.p6 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.c1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pirson2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.c1);
+            this.panel1.Controls.Add(this.c2);
+            this.panel1.Controls.Add(this.pirson2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.car2);
+            this.panel1.Controls.Add(this.car1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.p5);
+            this.panel1.Controls.Add(this.p3);
+            this.panel1.Controls.Add(this.p4);
+            this.panel1.Controls.Add(this.p6);
+            this.panel1.Controls.Add(this.p2);
+            this.panel1.Controls.Add(this.p1);
+            this.panel1.Location = new System.Drawing.Point(6, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1326, 555);
+            this.panel1.TabIndex = 0;
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.Yellow;
+            this.c1.Location = new System.Drawing.Point(21, 446);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(40, 44);
+            this.c1.TabIndex = 11;
+            this.c1.TabStop = false;
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.Yellow;
+            this.c2.Location = new System.Drawing.Point(1233, 446);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(38, 44);
+            this.c2.TabIndex = 10;
+            this.c2.TabStop = false;
+            this.c2.Click += new System.EventHandler(this.c2_Click);
+            // 
+            // pirson2
+            // 
+            this.pirson2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pirson2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pirson2.BackgroundImage")));
+            this.pirson2.Enabled = false;
+            this.pirson2.Location = new System.Drawing.Point(211, 378);
+            this.pirson2.Name = "pirson2";
+            this.pirson2.Size = new System.Drawing.Size(64, 125);
+            this.pirson2.TabIndex = 8;
+            this.pirson2.TabStop = false;
+            this.pirson2.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(551, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "game over";
+            this.label2.Visible = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.Color.Black;
+            this.car2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("car2.BackgroundImage")));
+            this.car2.Location = new System.Drawing.Point(1033, 98);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(63, 125);
+            this.car2.TabIndex = 7;
+            this.car2.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.BackColor = System.Drawing.Color.Black;
+            this.car1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("car1.BackgroundImage")));
+            this.car1.Location = new System.Drawing.Point(86, 109);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(61, 124);
+            this.car1.TabIndex = 6;
+            this.car1.TabStop = false;
+            this.car1.Click += new System.EventHandler(this.car1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(589, 378);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 125);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // p5
+            // 
+            this.p5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p5.Location = new System.Drawing.Point(618, 327);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(35, 185);
+            this.p5.TabIndex = 5;
+            this.p5.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p3.Location = new System.Drawing.Point(618, 22);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(35, 185);
+            this.p3.TabIndex = 4;
+            this.p3.TabStop = false;
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p4.Location = new System.Drawing.Point(86, 327);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(35, 185);
+            this.p4.TabIndex = 3;
+            this.p4.TabStop = false;
+            // 
+            // p6
+            // 
+            this.p6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p6.Location = new System.Drawing.Point(1033, 305);
+            this.p6.Name = "p6";
+            this.p6.Size = new System.Drawing.Size(35, 185);
+            this.p6.TabIndex = 2;
+            this.p6.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p2.Location = new System.Drawing.Point(1033, 22);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(35, 185);
+            this.p2.TabIndex = 1;
+            this.p2.TabStop = false;
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p1.Location = new System.Drawing.Point(99, 22);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(34, 185);
+            this.p1.TabIndex = 0;
+            this.p1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(1123, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "  score : 0  ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Enabled = false;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "  score two : 0  ";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(387, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 38);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "  coins 2 : 0  ";
+            this.label4.UseMnemonic = false;
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(746, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 38);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "  coins1 : 0  ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1355, 609);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.c1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pirson2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void car1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.PictureBox p6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pirson2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox c1;
+        private System.Windows.Forms.PictureBox c2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
